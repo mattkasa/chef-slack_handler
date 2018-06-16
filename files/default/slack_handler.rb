0574ccd455014fc85bf2c34e17a35f28cbf53ff6
Matt Kasa <mkasa@baent.net>
2018-06-16 03:11:30 -0700

    Attempt to fix run_status

--- files/default/slack_handler.rb
+++ files/default/slack_handler.rb
@@ -33,7 +33,6 @@ class Chef::Handler::Slack < Chef::Handler
 
   def initialize(config = {})
     Chef::Log.debug('Initializing Chef::Handler::Slack')
-    @util = SlackHandlerUtil.new(config)
     @config = config
     setup_slackr_options(@config)
 
@@ -45,9 +44,16 @@ class Chef::Handler::Slack < Chef::Handler
     @cookbook_detail_level = @config[:cookbook_detail_level]
   end
 
+  def setup_run_status(run_status)
+    @run_status = run_status
+    @util = SlackHandlerUtil.new(@config, @run_status)
+  end
+
   def report
+    setup_run_status(run_status)
+
     Timeout.timeout(@timeout) do
-      sending_to_slack = if run_status.is_a?(Chef::RunStatus)
+      sending_to_slack = if @run_status.is_a?(Chef::RunStatus)
                            report_chef_run_end
                          else
                            report_chef_run_start
@@ -75,15 +81,15 @@ class Chef::Handler::Slack < Chef::Handler
 
   def report_chef_run_start
     return false unless @util.send_on_start
-    slack_message(@util.start_message.to_s, run_status.node.name)
+    slack_message(@util.start_message.to_s, @run_status.node.name)
   end
 
   def report_chef_run_end
-    if run_status.success?
+    if @run_status.success?
       return false if @util.fail_only
-      slack_message("#{@util.end_message(run_status)} \n #{run_status.exception}", run_status.node.name)
+      slack_message("#{@util.end_message(@run_status)} \n #{@run_status.exception}", @run_status.node.name)
     else
-      slack_message(@util.end_message(run_status).to_s, run_status.node.name)
+      slack_message(@util.end_message(@run_status).to_s, @run_status.node.name)
     end
   end
 
